2cab68a52eeca5369187f808f2d30f56d5a973c2
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-01 18:19:53 +0530

    Welcome to Quantity Measurement

--- QuantityMeasurement/README.md
+++
@@ -1,2 +0,0 @@
-# QuantityMeasurement
-Emphasis on TDD for Developing Quantity Measurement System,Applying OO Concepts and Design Principles and Using Mockito to Mock DependentObjects.
--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -1,10 +1,3 @@
-import org.junit.Test;
-import quantitymeasurement.QuantityMeasurement;
-
 public class QuantityMeasurementTest {
-
-    @Test
-    public void given0Feetand0Feet_shouldReturnEqual() {
-        QuantityMeasurement quantityMeasurement =new QuantityMeasurement();
-    }
+    
 }
