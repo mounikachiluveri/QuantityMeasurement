4c71e0112707d36c39ac57f2d6deb679614814ab
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-01 22:25:13 +0530

    Added: Testcase value check for feet

--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -9,17 +9,17 @@ public class QuantityMeasurementTest {
     @Test
     public void given0FEETand0FEET_shouldReturnEqual() {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        double inch1 = quantityMeasurement.returnUnit(UnitType.FEET, 0.0);
-        double inch2 = quantityMeasurement.returnUnit(UnitType.FEET, 0.0);
-        Assert.assertEquals(inch1, inch2, 0.0);
+        double Feet1 = quantityMeasurement.returnUnit(UnitType.FEET, 0.0);
+        double Feet2 = quantityMeasurement.returnUnit(UnitType.FEET, 0.0);
+        Assert.assertEquals(Feet1, Feet2, 0.0);
     }
 
     @Test
     public void given0FEETand1FEET_IfNotEqual_shouldReturnNotEqual() {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        double inch1 = quantityMeasurement.returnUnit(UnitType.FEET, 0.0);
-        double inch2 = quantityMeasurement.returnUnit(UnitType.FEET, 1.0);
-        Assert.assertNotEquals(inch1, inch2, 0.0);
+        double Feet1 = quantityMeasurement.returnUnit(UnitType.FEET, 0.0);
+        double Feet2 = quantityMeasurement.returnUnit(UnitType.FEET, 1.0);
+        Assert.assertNotEquals(Feet1, Feet2, 0.0);
     }
 
     @Test
@@ -37,6 +37,13 @@ public class QuantityMeasurementTest {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
         boolean equals = quantityMeasurement.equals(quantityMeasurement);
         Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void givenQuantityMeasurementReference_IfNotProper_ShouldReturnFalse() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        boolean equals = quantityMeasurement.equals(quantityMeasurement);
+        Assert.assertFalse(equals);
     }
 
     @Test
@@ -60,6 +67,29 @@ public class QuantityMeasurementTest {
         boolean equals = quantityMeasurement.equals(new QuantityMeasurementException("NULL",
                 QuantityMeasurementException.ExceptionType.NULL_VALUE));
         Assert.assertFalse(equals);
+    }
+
+    @Test
+    public void given0Inchand0Inch_shouldReturnEqual() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double inch1 = quantityMeasurement.returnUnit(UnitType.INCH, 0.0);
+        double inch2 = quantityMeasurement.returnUnit(UnitType.INCH, 0.0);
+        Assert.assertEquals(inch1, inch2, 0.0);
+    }
+    @Test
+    public void given0inchand1inch_IfNotEqual_shouldReturnNotEqual() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double Feet1 = quantityMeasurement.returnUnit(UnitType.INCH, 0.0);
+        double Feet2 = quantityMeasurement.returnUnit(UnitType.INCH, 1.0);
+        Assert.assertNotEquals(Feet1, Feet2, 0.0);
+    }
+
+    @Test
+    public void givenValueCheckForFeet_IfEqual_ShouldReturnTrue() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double feet1 = quantityMeasurement.returnUnit(UnitType.FEET, 4.2);
+        double feet2 = quantityMeasurement.returnUnit(UnitType.FEET, 4.2);
+        Assert.assertEquals(feet1, feet2, 0.0);
     }
 }
 
