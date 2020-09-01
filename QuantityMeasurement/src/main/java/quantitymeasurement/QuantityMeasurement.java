7d822fb00bbdd5fba35f436cbe02a0933b2f9f2a
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-01 19:16:36 +0530

    Added:Test case given 1 feet and 12 inches return equal

--- QuantityMeasurement/src/main/java/quantitymeasurement/QuantityMeasurement.java
+++ QuantityMeasurement/src/main/java/quantitymeasurement/QuantityMeasurement.java
@@ -1,7 +1,7 @@
 package quantitymeasurement;
 
 public class QuantityMeasurement {
-    public double returnUnit(double conversation, double unit) {
-        return conversation * unit;
+    public double returnUnit(UnitType type, double unit) {
+        return  unit * type.conversation;
     }
 }
--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -1,24 +1,33 @@
 import org.junit.Assert;
 import org.junit.Test;
 import quantitymeasurement.QuantityMeasurement;
+import quantitymeasurement.UnitType;
 
 public class QuantityMeasurementTest {
 
     @Test
     public void given0Feetand0Feet_shouldReturnEqual() {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        double inch = quantityMeasurement.returnUnit(12.0, 0.0);
-        Assert.assertEquals(inch, 0.0, 0.0);
+        double inch1 = quantityMeasurement.returnUnit(UnitType.Feet, 0);
+        double inch2 = quantityMeasurement.returnUnit(UnitType.Feet, 0);
+        Assert.assertEquals(inch1,inch2,0.0);
     }
 
     @Test
     public void given0Feetand1Feet_IfNotEqual_shouldReturnNotEqual() {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        double inch1 = quantityMeasurement.returnUnit(12.0, 0.0);
-        double inch2 = quantityMeasurement.returnUnit(12.0, 1.0);
+        double inch1 = quantityMeasurement.returnUnit(UnitType.Feet, 0.0);
+        double inch2 = quantityMeasurement.returnUnit(UnitType.Feet, 1.0);
         Assert.assertNotEquals(inch1, inch2, 0.0);
     }
-}
 
+    @Test
+    public void given1Feettand12Inches_IfNotEqual_shouldReturnNotEqual() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double inch1 = quantityMeasurement.returnUnit(UnitType.Feet,1);
+        double inch2 = quantityMeasurement.returnUnit(UnitType.Inch, 12.0);
+        Assert.assertEquals(inch1, inch2, 0.0);
+    }
+}
 
 
