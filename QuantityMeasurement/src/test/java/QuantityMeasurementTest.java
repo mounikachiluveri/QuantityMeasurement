bd607ca683c177112c5b8059c9febf5f0a3bbce1
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-01 18:46:52 +0530

    Added:Test case to 0 feet return 1 Feet return notequal

--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -10,6 +10,15 @@ public class QuantityMeasurementTest {
         double inch = quantityMeasurement.returnUnit(12.0, 0.0);
         Assert.assertEquals(inch, 0.0, 0.0);
     }
+
+    @Test
+    public void given0Feetand1Feet_IfNotEqual_shouldReturnNotEqual() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double inch1 = quantityMeasurement.returnUnit(12.0, 0.0);
+        double inch2 = quantityMeasurement.returnUnit(12.0, 1.0);
+        Assert.assertNotEquals(inch1, inch2, 0.0);
+    }
 }
+
 
 
