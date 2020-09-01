26d6104fe5764a0c3e4234de318baa113c60ef5c
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-01 18:22:44 +0530

    Added:Test case for Quantity Measurement

--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+import quantitymeasurement.QuantityMeasurement;
+
 public class QuantityMeasurementTest {
-    
+
+    @Test
+    public void given0Feetand0Feet_shouldReturnEqual() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+    }
 }
+
+
