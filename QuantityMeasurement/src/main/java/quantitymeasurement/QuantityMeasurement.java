8ad41bb3bbbee0f782d9a47ad33a3047ee57ec7a
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-03 18:51:44 +0530

    Refactered:code to creates the equal method and Test case added to check equality of 2 feets lengths

--- QuantityMeasurement/src/main/java/quantitymeasurement/QuantityMeasurement.java
+++
@@ -1,9 +0,0 @@
-package quantitymeasurement;
-
-public class QuantityMeasurement {
-    public double value;
-
-    public double returnUnit(UnitType type, Double unit) {
-        return unit * type.conversation;
-    }
-}
