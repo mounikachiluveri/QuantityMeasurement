7298e178240f3e44c5849f9c1e29c7645fcd46fc
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-03 19:02:04 +0530

    Added:Test case to compare 1 Feet not equal to 2 Feet

--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -10,4 +10,11 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void given1FeetAnd2Feet_IfNotEqual_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(2.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
 }
