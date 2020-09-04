dccb5ed8acac9ccf46cf519b1f06a7ac3fab905b
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-04 20:37:01 +0530

    given 2 Inches added to 2.5 Centimetres Equal to 3 Inches

--- QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/QuantityMeasurementTest.java
@@ -209,11 +209,29 @@ public class QuantityMeasurementTest {
     }
 
     @Test
-    public void given1FeetAnd2Inches_WhenEqualTo14Inches_ShouldReturnTrue() {
+    public void given2InchAnd1Feet_WhenAddedEqualTo14Inches_ShouldReturnTrue() {
+        double inch1 = quantityMeasurement.calculateUnit(UnitType.INCH, 2.0);
+        double inch2 = quantityMeasurement.calculateUnit(UnitType.FEET, 1.0);
+        double inch3 = quantityMeasurement.addition(inch1, inch2);
+        boolean compare = quantityMeasurement.compare(14.0, inch3);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void given1FeetAnd1Feet_WhenAddedEqualTo24Inches_ShouldReturnTrue() {
         double feet1 = quantityMeasurement.calculateUnit(UnitType.FEET, 1.0);
         double feet2 = quantityMeasurement.calculateUnit(UnitType.FEET, 1.0);
         double inch3 = quantityMeasurement.addition(feet1, feet2);
         boolean compare = quantityMeasurement.compare(24.0, inch3);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void given2InchesAnd2point5Centimetre_WhenAddedEqualTo3Inches_ShouldReturnTrue() {
+        double inch1 = quantityMeasurement.calculateUnit(UnitType.INCH, 2.0);
+        double inch2 = quantityMeasurement.calculateUnit(UnitType.CENTIMETRE, 2.5);
+        double inch3 = quantityMeasurement.addition(inch1, inch2);
+        boolean compare = quantityMeasurement.compare(3.0, inch3);
         Assert.assertTrue(compare);
     }
 }
